23a25d25027015d9606be8d5b9f7ffd1fd6c672f
Olivier Fondeur <fondeurolivier14@gmail.com>
2025-03-10 10:53:19 -0700

    prediction now work from ./scripts/predict.py with ./scripts/predict.py --result_path ./results/LPS_cMC/model-cellot --unstim_data_path ./results/LPS_cMC/model-cellot/unseen_data/combined_LPS_cMC_HCAA2.h5ad --output_path ./results/LPS_cMC/model-cellot/unseen_data/pred.h5ad Prediction saved under: /Users/MacBook/stanford/cellot/results/LPS_cMC/model-cellot/unseen_data/pred.h5ad

--- scripts/predict.py
+++ scripts/predict.py
@@ -1,10 +1,11 @@
-import sys
 from absl import app, flags
 from pathlib import Path
-import yaml
 from make_prediction import predict_from_unstim_data
+from pathlib import Path
+from absl import app, flags
 
 FLAGS = flags.FLAGS
+
 flags.DEFINE_string(
     "result_path", None, "Path to the trained model directory (e.g., ./results/...)"
 )
@@ -13,36 +14,35 @@ flags.DEFINE_string(
     None,
     "Path to the unstim data file (e.g., ./datasets/unstim_data.h5ad)",
 )
+flags.DEFINE_string("output_path", None, "Path to save the predictions (optional)")
 flags.DEFINE_string(
-    "output_path",
-    None,
-    "Path to save the predictions (e.g., ./predictions/output.h5ad)",
+    "pred_format", "h5ad", "Format for saving the predictions (csv or h5ad)"
 )
 
 
 def main(argv):
     del argv
-    if (
-        FLAGS.result_path is None
-        or FLAGS.unstim_data_path is None
-        or FLAGS.output_path is None
-    ):
-        raise ValueError(
-            "You need to give the 3 args --result_path, --unstim_data_path and --output_path"
-        )
+
+    if not FLAGS.result_path or not FLAGS.unstim_data_path:
+        raise ValueError("You need to specify --result_path and --unstim_data_path")
 
     result_path = Path(FLAGS.result_path).resolve()
     unstim_data_path = Path(FLAGS.unstim_data_path).resolve()
-    output_path = Path(FLAGS.output_path).resolve()
+    output_path = Path(FLAGS.output_path).resolve() if FLAGS.output_path else None
 
     if not result_path.exists():
-        raise FileNotFoundError(f"Result path does not exist : {result_path}")
+        raise FileNotFoundError(f"Result path does not exist: {result_path}")
     if not unstim_data_path.exists():
-        raise FileNotFoundError(f"Data path does not exist : {unstim_data_path}")
+        raise FileNotFoundError(f"Data path does not exist: {unstim_data_path}")
 
-    predict_from_unstim_data(result_path, unstim_data_path, output_path)
+    prediction = predict_from_unstim_data(
+        result_path, unstim_data_path, FLAGS.pred_format, output_path
+    )
 
-    print(f"Prediction made and saved under : {output_path}")
+    if output_path:
+        print(f"Prediction saved under: {output_path}")
+    else:
+        print("Prediction completed but not saved (no output path specified)")
 
 
 if __name__ == "__main__":
